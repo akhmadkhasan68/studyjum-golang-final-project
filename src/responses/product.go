579c39ae435d496eac6ae2588d1f2ab8d79670c7
Akhmad Khasan Abdullah <41146877+akhmadkhasan68@users.noreply.github.com>
2023-02-15 20:34:56 +0700

    response product

--- src/responses/product.go
+++ src/responses/product.go
@@ -6,13 +6,13 @@ import (
 )
 
 type ProductResponse struct {
-	ID          string       `json:"id"`
-	Name        string       `json:"name"`
-	Description string       `json:"description"`
-	Price       float64      `json:"price"`
-	User        UserResponse `json:"user"`
-	CreatedAt   string       `json:"created_at"`
-	UpdatedAt   string       `json:"updated_at"`
+	ID          string  `json:"id"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Price       float64 `json:"price"`
+	Outlet      string  `json:"outlet"`
+	CreatedAt   string  `json:"created_at"`
+	UpdatedAt   string  `json:"updated_at"`
 }
 
 func ToProductsResponse(products *[]models.Product) []ProductResponse {
@@ -23,21 +23,9 @@ func ToProductsResponse(products *[]models.Product) []ProductResponse {
 			Name:        value.Name,
 			Description: value.Description,
 			Price:       value.Price,
-			User: UserResponse{
-				Username:    value.Outlet.Username,
-				Email:       value.Outlet.Email,
-				PhoneNumber: value.Outlet.PhoneNumber,
-				FirstName:   value.Outlet.FirstName,
-				LastName:    value.Outlet.LastName,
-				Address:     value.Outlet.Address,
-				AreaID:      value.Outlet.AreaID,
-				Longitude:   value.Outlet.Longitude,
-				Latitude:    value.Outlet.Latitude,
-				CreatedAt:   value.Outlet.CreatedAt.Format(constants.DateTimeLayout),
-				UpdatedAt:   value.Outlet.UpdatedAt.Format(constants.DateTimeLayout),
-			},
-			CreatedAt: value.CreatedAt.Format(constants.DateTimeLayout),
-			UpdatedAt: value.UpdatedAt.Format(constants.DateTimeLayout),
+			Outlet:      value.Outlet.FirstName + " " + value.Outlet.LastName,
+			CreatedAt:   value.CreatedAt.Format(constants.DateTimeLayout),
+			UpdatedAt:   value.UpdatedAt.Format(constants.DateTimeLayout),
 		})
 	}
 
@@ -50,20 +38,8 @@ func ToProductResponse(product *models.Product) ProductResponse {
 		Name:        product.Name,
 		Description: product.Description,
 		Price:       product.Price,
-		User: UserResponse{
-			Username:    product.Outlet.Username,
-			Email:       product.Outlet.Email,
-			PhoneNumber: product.Outlet.PhoneNumber,
-			FirstName:   product.Outlet.FirstName,
-			LastName:    product.Outlet.LastName,
-			Address:     product.Outlet.Address,
-			AreaID:      product.Outlet.AreaID,
-			Longitude:   product.Outlet.Longitude,
-			Latitude:    product.Outlet.Latitude,
-			CreatedAt:   product.Outlet.CreatedAt.Format(constants.DateTimeLayout),
-			UpdatedAt:   product.Outlet.UpdatedAt.Format(constants.DateTimeLayout),
-		},
-		CreatedAt: product.CreatedAt.Format(constants.DateTimeLayout),
-		UpdatedAt: product.UpdatedAt.Format(constants.DateTimeLayout),
+		Outlet:      product.Outlet.FirstName + " " + product.Outlet.LastName,
+		CreatedAt:   product.CreatedAt.Format(constants.DateTimeLayout),
+		UpdatedAt:   product.UpdatedAt.Format(constants.DateTimeLayout),
 	}
 }
